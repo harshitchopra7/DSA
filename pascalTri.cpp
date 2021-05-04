254d01da1059f8b135c067082ebd20cc3a00d8a2
Harshit Chopra <52441396+harshitchopra7@users.noreply.github.com>
2021-05-04 23:47:07 +0530

    Add files via upload

---
+++ pascalTri.cpp
@@ -0,0 +1,35 @@
+/*
+Pascal Triangle
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int factorial (int num) {
+	int fact = 1;
+	for (int i=2; i<=num; i++) {
+		fact*=i;
+	}
+	return fact;
+}
+
+int main () {
+	int n;
+	cout<<"Enter row no. \n";
+	cin>>n;
+
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<=i; j++) {
+			cout<<factorial(i) / (factorial(i-j) * factorial(j))<<" ";
+		}
+		cout<<endl;
+	}
+
+	return 0;
+}
