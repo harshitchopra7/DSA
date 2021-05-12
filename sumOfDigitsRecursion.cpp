82bc9b197851038ec84d3d540032b7638e115c3e
Harshit Chopra <52441396+harshitchopra7@users.noreply.github.com>
2021-05-12 17:22:58 +0530

    Add files via upload

---
+++ sumOfDigitsRecursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int sumOfDigits (int n) {
+	if (n==0) 
+		return 0;
+	return ((n%10) + sumOfDigits(n/10));
+}
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<sumOfDigits(n);
+	return 0;
+}
