3adc882e2dc3913c2021ccd254d634855ad8589a
Harshit Chopra <52441396+harshitchopra7@users.noreply.github.com>
2021-05-30 13:34:14 +0530

    Add files via upload

---
+++ preorderBinaryTreeTraversal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int key;
+	Node *left;
+	Node *right;
+	Node(int k) {
+		key = k;
+		left = right = NULL;
+	}	
+};
+
+void preorder(Node *root) {
+	if (root != NULL) {
+		cout<<(root -> key)<<" ";
+		preorder(root -> left);
+		preorder(root -> right);
+	}
+}
+
+int main() {
+	Node *root = new Node(10);
+	root -> left = new Node(20);
+	root -> right = new Node(30);
+	root -> left -> left = new Node(40);
+	root -> left -> right = new Node(50);
+
+	preorder(root);
+
+	return 0;
+}
