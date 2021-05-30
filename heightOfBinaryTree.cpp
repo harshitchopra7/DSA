fcbe7a44849865a29bb1ba741ed8629d12518191
Harshit Chopra <52441396+harshitchopra7@users.noreply.github.com>
2021-05-30 13:35:12 +0530

    Add files via upload

---
+++ heightOfBinaryTree.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+	int key;
+	Node *left;
+	Node *right;
+	Node(int k) {
+		key = k;
+		left = right = NULL;
+	}	
+};
+
+int height(Node *root) {
+	if (root == NULL)
+		return 0;
+	else
+		return max(height(root -> left), height(root -> right)) + 1;
+}
+
+int main() {
+	Node *root = new Node(10);
+	root -> left = new Node(20);
+	root -> right = new Node(30);
+	root -> left -> left = new Node(40);
+	root -> left -> right = new Node(50);
+
+	cout<<height(root);
+
+	return 0;
+}
