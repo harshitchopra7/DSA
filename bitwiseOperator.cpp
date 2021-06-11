fb6d40f17d74260be9d753934d65a79f5f113396
Harshit Chopra <52441396+harshitchopra7@users.noreply.github.com>
2021-06-11 13:25:59 +0530

    Add files via upload

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 2;
+	int b = 3;
+	int c = a & b;
+	int d = a | b;
+	int e = a ^ b;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	cout<<e<<endl;
+	return 0;
+}
